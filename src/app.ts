51370f5784b4ed2fd31efeaf798393f3a628bf59
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-11-15 11:14:49 +0100

    init project: start server + env.variables

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import { envs } from './config';
+
+(()=>{
+    main();
+})();
+
+function main(){
+    const app = express();
+
+    app.post('/api/github',(req, res)=>{
+        res.json('Github Endpoint');
+    });
+
+    app.listen(envs.PORT, ()=> {
+        console.log(`App running on PORT:${envs.PORT}`);
+    })
+}
+
+
