f67f9405fa3d2bea729525cd27420f1ee61c544f
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-11-15 15:40:10 +0100

    setting-up: discord server & bots

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,5 +4,6 @@ import {get} from 'env-var';
 export const envs = {
     PORT: get('PORT').required().asPortNumber(),
     GITHUB_WEBHOOK_SECRET: get('GITHUB_WEBHOOK_SECRET').required().asString(),
+    DISCORD_WEBHOOK_URL: get('DISCORD_WEBHOOK_URL').required().asString(),
 }
 
